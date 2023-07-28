8bbce7da4f2e6779106dd3b4b0e8261fbdd4d419
Junjie Chen <chen2468830035@gmail.com>
2023-07-29 00:48:44 +0200

    up

---
+++ js/Register.js
@@ -0,0 +1,32 @@
+﻿var username = "";
+var password = "";
+const uri = 'http://sep-db-386814.ew.r.appspot.com/user/add/User';
+
+function Register() {
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
+  const User = {
+    "username": username,
+    "password": password
+  }
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', uri, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send(JSON.stringify(User))
+  xhr.onreadystatechange = () => {
+    //获取响应内容
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      alert("Register successful")
+      window.location.href = "../login.html";
+    }
+  }
+
+}
+  document.getElementById('register-form').addEventListener('submit', function(event) {
+    Register();
+  })
+
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let user = document.getElementById('lg');
+
+user.onclick=()=>checkProfile();
+
+
+function checkProfile(){
+  let username = localStorage.getItem('username1');
+  if (username===null){
+    window.location.href = "../login.html";
+  }
+  else {
+    window.location.href = "html/profile.html";
+  }
+}
