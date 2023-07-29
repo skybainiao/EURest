8f19ed230bd93fc0809eedaea29ae2c7feeb29fe
Junjie Chen <chen2468830035@gmail.com>
2023-07-30 01:52:38 +0200

    up

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
 
-
-let user = document.getElementById('lg');
-
-user.onclick=()=>checkProfile();
-
-
-
-function checkProfile(){
-  let username = localStorage.getItem('username1');
-  if (username===null){
-    window.location.href = "../html/login.html";
-  }
-  else {
-    window.location.href = "html/profile.html";
-  }
-}
-
